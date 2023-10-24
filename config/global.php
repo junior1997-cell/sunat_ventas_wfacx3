095e16b015588e7ac8b13c3e2ff85ec5e30d42bd
Junior Cercado Vasquez <juniorcercado@upeu.edu.pe>
2023-10-23 20:24:54 -0500

    conexion de bd

--- config/global.php
+++ config/global.php
@@ -2,7 +2,7 @@
 define("DB_HOST","localhost");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "");
-define("DB_NAME", "sunat_ventas_wfacx2");
+define("DB_NAME", "sunat_ventas_wfacx3");
 define("DB_ENCODE","utf8");
 define("PRO_NOMBRE","SISTEMA FACX");
 ?>
